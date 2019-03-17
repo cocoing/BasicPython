59b72fb3f18dc5a4dec2d9fd5acd9dec31f5197a
cocoing <715068392@qq.com>
2019-03-17 23:33:39 +0800

    add guess birthday file

--- test.py
+++ test.py
@@ -1,8 +1,26 @@
 # this is a test python file, you can modify the content to test some code
-
+#!/usr/bin/python3
 """
-# format 
+# format practise
 age = 38
 name = "little ming"
 print("{0:*<20s} is {1:=>6d} years old now".format(name, age))
 """
+
+# who's birthday is coming
+birthdays = {'Alice':'Apr 1', 'Bob':'Dec 12', 'Carol':'Mar 4'}
+
+while True:
+    print("Enter a name(blank to quit):")
+    name = input()
+    if name == '':
+        break
+
+    if name in birthdays:
+        print("{} is the birthday of {}".format(birthdays[name], name))
+    else:
+        print("I do not have birthday information for {}".format(name))
+        bday = input()
+        birthdays[name] = bday
+        print("Birthday database updated".center(50,'*'))
+    
